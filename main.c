2e29875e78b8daddd80ecf06aed93b613cfb6150
Lukasz <sobonukasz@gmail.com>
2022-01-29 18:44:55 +0100

    Zmiana dodawania na mnożenie w funkcji count

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 
 int count (int a, int b)
 {
-    return a+b;
+    return a*b;
 }
 
 int main() {
     printf("Hello, World!\n");
-    printf(" W pierwszym branchu; 2 + 3 = %d\n", count(2, 3));
+    printf(" W drugim branchu; 2 * 3 = %d\n", count(2, 3));
     return 0;
 }

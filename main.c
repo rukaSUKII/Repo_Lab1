0a75e53f04e78a4abb98974d35e453cbcb1ad55e
Lukasz <sobonukasz@gmail.com>
2022-01-29 18:41:50 +0100

    Nowa funkcja count

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+int count (int a, int b)
+{
+    return a+b;
+}
+
 int main() {
     printf("Hello, World!\n");
+    printf(" W pierwszym branchu; 2 + 3 = %d\n", count(2, 3));
     return 0;
 }

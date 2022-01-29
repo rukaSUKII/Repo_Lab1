0b33527ee0d54b2b14288e454d6ea381dcd5810f
Lukasz <sobonukasz@gmail.com>
2022-01-29 18:39:35 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Repo_Lab1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Repo_Lab1.dir/main.c.obj"
+  "Repo_Lab1.exe"
+  "Repo_Lab1.exe.manifest"
+  "Repo_Lab1.pdb"
+  "libRepo_Lab1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Repo_Lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
